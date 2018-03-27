fad48d1863593a0bd084c9fbef77a28b4f48288e
Metaphysiker <s.raess@me.com>
2018-03-27 22:22:48 +0200

    inserted html documents

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,24 @@ class StaticPagesController < ApplicationController
   def welcome
 
   end
+
+  def research
+
+  end
+
+  def teaching
+
+  end
+
+  def workshops
+
+  end
+
+  def services
+
+  end
+
+  def nonacademicstuff
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#welcome'
+
+  get '/research', to: 'static_pages#research'
+  get '/teaching', to: 'static_pages#teaching'
+
+  get '/workshops', to: 'static_pages#workshops'
+
+  get '/services', to: 'static_pages#services'
+
+  get '/nonacademicstuff', to: 'static_pages#nonacademicstuff'
 end
