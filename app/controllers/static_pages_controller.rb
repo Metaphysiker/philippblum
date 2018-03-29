f946eec5c892f9d62b4f5035b125b0a7590d03c2
Metaphysiker <s.raess@me.com>
2018-03-29 14:17:17 +0200

    added seed and posts for sections

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
   def welcome
-
+    @post = Post.find_by_section("welcome")
   end
 
   def research
-
+    @post = Post.find_by_section("research")
   end
 
   def teaching
@@ -12,11 +12,11 @@ class StaticPagesController < ApplicationController
   end
 
   def workshops
-
+    @post = Post.find_by_section("workshops")
   end
 
   def services
-
+    @post = Post.find_by_section("services")
   end
 
   def nonacademicstuff
