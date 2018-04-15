0d0204f2629deda17d2203b185624c13737eddbf
Metaphysiker <s.raess@me.com>
2018-04-15 09:09:26 +0200

    add track_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  after_action :track_action
 
   def user_allowed?
     if current_user.nil? || !current_user.is_admin?
       redirect_to root_path
     end
+  end
+
+
+
+  protected
+
+  def track_action
+    ahoy.track "Ran action", request.path_parameters
   end
 
 end
